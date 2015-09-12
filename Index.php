90450851a265d92bf19b0f0138a6a92e1d220809
ludivinecp <coipel.ludivine@gmail.com>
2015-09-12 18:06:13 +0200

    first commit

---
+++ Index.php
@@ -0,0 +1,445 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8"/>
+	
+	
+	<link rel="stylesheet" href="style.css" @media="screen and (max-width:900px)/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+
+
+  	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+	  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	  <link rel="stylesheet" href="/resources/demos/style.css">
+
+   <script>
+  $(function() {
+    $( "#accordion" ).accordion();
+  });
+  </script>
+	
+	<title>Prix des start-up normaliennes</title>
+</head>
+
+
+
+
+
+<body>
+<div class="container-fluid">
+	
+<!-- $HEADER -->
+	
+		<nav>
+			<ul>
+				<a href="#actualites"><li>Actualités</li></a>
+				<a href="#Concours"><li>Concours</li></a>
+				<a href="#Laureats"><li>Lauréats</li></a>
+				<a href="#Partenaires"><li>Partenaires</li></a>
+				<a href="#FAQ"><li>FAQ</li></a>
+				<a href="#Contact"><li>Contact</li></a>
+			</ul>
+		</nav>
+
+		<h1>Prix des start-up normaliennes</h1>
+
+		<img src="header.jpg"alt="photo header"/>
+
+	<div class="wrap">
+
+
+<!-- $ACTUALITES -->
+
+		<section id="actualites">	
+			<h2 class="titre_partie">Les dernières actualités</h2>
+			<h3 class="sous_titre_partie">Actualités archivées</h3>
+
+				<!-- Slider -->
+
+
+
+
+			 
+<!-- $Concours -->
+
+		<div class="col-md-12"> 
+			<section id="concours">
+			  		<h2 class="titre_partie2">Concours</h2>
+
+                <!-- contenu... -->                 
+                Lorem ipsum dolor sit amet,
+				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+				reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+				
+			</section>
+ 		</div>		 
+
+
+
+
+<!-- $LAUREATS -->
+
+		<section id="laureats">
+			<div class="row">
+				<div class="col-md-12">
+				  	<h2 class="titre_partie">Les lauréats du concours 2015</h2>
+				  			<h3 class="sous_titre_partie"> Les anciens lauréats</h3>
+		  		</div>
+		  	</div>
+
+		  	<!--colonne vide -->
+		  	<div class="row">
+		  		<div class="col-md-1">
+		  		</div>
+		  		
+		  		<!--colonne lauréat -->
+			  	<div class="col-md-4 colonne_laureats">
+		  			<img src="laureats.jpg" alt="photo lauréat"/>
+		  			<h4>Nom des Lauréats</h4>
+			  			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+			  			</p>
+
+		  					<!-- Button modal -->
+							<button type="button" class="btn" data-toggle="modal" data-target="#myModal">
+							  <p> En savoir plus </p>S
+							</button>
+
+							<!-- Modal -->
+							<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+							  <div class="modal-dialog" role="document">
+							    <div class="modal-content">
+							      <div class="modal-header">
+							        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							        <h4 class="modal-title" id="myModalLabel">Lauréat</h4>
+							      </div>
+							      <div class="modal-body">
+
+							      	<img src="laureats.jpg" alt="photo lauréat"/>
+
+								      <p>Présentation des candidats :</br>
+								      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+		  								</p>
+							     
+							      </div>
+							      <div class="modal-footer">
+							        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+							        
+							      </div>
+							    </div>
+							  </div>
+							</div>
+		  		</div>
+
+		  		<!--colonne vide -->
+	  			<div class="col-md-2">
+	  			</div>
+
+
+		  		<div class="col-md-4 colonne_laureats">
+		  			<img src="laureats.jpg" alt="photo lauréat"/>
+		  			<h4>Nom des Lauréats</h4>
+		  				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+		  				</p>
+
+							<!-- Button modal -->
+							<button type="button" class="btn" data-toggle="modal" data-target="#myModal">
+							  En savoir plus
+							</button>
+
+							<!-- Modal -->
+							<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+							  <div class="modal-dialog" role="document">
+							    <div class="modal-content">
+							      <div class="modal-header">
+							        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							        <h4 class="modal-title" id="myModalLabel">Lauréat</h4>
+							      </div>
+							      <div class="modal-body">
+
+							      	<img src="laureats.jpg" alt="photo lauréat"/>
+
+								      <p>Présentation des candidats : </br>
+								      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+		  								</p>
+							     
+							      </div>
+							      <div class="modal-footer">
+							        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+							        
+							      </div>
+							    </div>
+							  </div>
+							</div>
+
+		  		</div>
+
+
+		  		<!--colonne vide -->
+	  			<div class="col-md-1">
+	  			</div>
+
+		  	</div>
+
+
+		</section>
+
+
+
+
+
+<!-- $Partenaires -->
+			<section id="Partenaires">
+			  	<h2 class="titre_partie2">Partenaires</h2>
+			  		
+			  	
+			  	 <!-- contenu... -->                 
+               
+
+			</section>
+		</div>
+
+
+
+
+
+
+<!-- $FAQ -->
+
+		<section id="FAQ">
+			<h2 class="titre_partie2">FAQ</h2>
+
+				<div id="accordion">
+				  <h3>Ce concours de startups s’adresse à la “communauté normalienne au sens large”. Mais, concrètement, 
+				  qui peut participer ?</h3>
+					  <div>
+					    <p> 
+						    Il est nécessaire qu’un membre de l’équipe opérationnelle du projet (pas du board) soit 
+						    membre de la communauté normalienne au sens large:
+							aux normaliens et normaliennes, de toutes les écoles normales;
+							fonctionnaire stagiaire (ou “élève normalien”)
+							admis à préparer le diplôme de l'ENS (“étudiant normalien” depuis peu)
+							de la sélection internationale étranger à la préparation de l'agrégation
+							ancien ayant eu un de ces statuts
+					    </p>
+					  </div>
+				 
+
+				  <h3>Pourquoi un concours réservé aux normaliens ?</h3>
+					  <div>
+					    <p>
+					    
+						Le CNE cherche deux choses:
+							<ul>
+								<li>sensibiliser des membres du CNE à l’économie des startups ;</li>
+								<li>permettre à des projets de pouvoir être testés dans un environnement bienveillant ;</li>
+							</ul>
+						Certes, les normaliens ne sont pas les plus mal armés pour affronter le monde de l’innovation mais,
+						en même temps, ils peuvent manquer de confiance en eux. Ils peuvent aussi manquer d’un nécessaire sens de 
+						la communication. Ce concours a pour fin de les aider à essayer, tenter, expérimenter.
+
+					 	</p>
+					  </div>
+				 
+
+				  <h3>Qu’est-ce qu’un pecha kucha ? Pourquoi avez-vous choisi ce format de concours ?</h3>
+					  <div>
+					    <p>
+					    Pour en savoir plus sur ce format, rendez-vous ici. Effectivement, les dix finalistes (s’ils sont 10! 
+					    ’est un maximum), se livreront à un pechakucha, qui est une présentation orale devant des images dont le 
+					    mps de défilement est fixe. Ce format a plusieurs avantages. Toute d’abord, il permet de maîtriser le 
+					    temps de présentation. Ensuite, c’est un format qui permet de rendre les présentations des différents 
+					    candidats plus “comparables” entre elles.
+					    </p>
+					  </div>
+
+
+				 <h3>Pourquoi offrir du mentorat comme prix ? Pourquoi pas du financement ?</h3>
+					  <div>
+					    <p>
+					    Il y a beaucoup de concours de startups, Et la France est un territoire où des nombreux dispositifs aident 
+					    la création d’entreprise. Mais ce qui manque souvent à l’entrepreneur, c’est du réseau, et de l’expérience.
+					    Le mentorat apporte cela. En outre, ce concours est aussi ouvert à des projet intrapreneuriaux, qui ont 
+					    vocation à rester dans le giron du groupe privé ou de l’institution publique qui les produit. Ces projets 
+					    ont tous autant besoin de mentorat - mais leur besoin d’investissement financier se pose en termes différents.
+					    </p>
+					  </div>
+
+  				<h3>Mais qu’est-ce que le CNE ?</h3>
+					  <div>
+					    <p> 
+						 Le CNE est le Club des Normaliens dans l’entreprise. Il comprend de élèves et anciens élèves de l’Ecole 
+						 normale supérieure de Paris. Pour en savoir plus, c’est par ici.
+					    </p>
+					  </div>
+				 
+
+				  <h3>Qui sera au jury ?</h3>
+					  <div>
+					    <p>
+						Ce sera une surprise. Pour vous. Mais pour nous aussi, car nous sommes en train de constituer ce jury.
+					 	</p>
+					  </div>
+				 
+
+				  <h3>Vous cherchez des projets innovants ? Mais qu’est-ce que l’innovation ?</h3>
+					  <div>
+					    <p>
+					    Vaste sujet. Ce n’est pas cette FAQ qui lui apportera une réponse. Cela étant, pour ce concours, une 
+					    précision est nécessaire : l’innovation n’est pas que technologique, elle peut être d’usage, ou économique.
+					    </p>
+					  </div>
+
+
+				  <h3>Seules les startups sont invitées à concourir. Mais qu’est ce qu’une startup ?</h3>
+					  <div>
+					    <ul>
+					    	<li>présence d'une innovation (technologique, usage, économique) ;</li>
+					    	<li>jusqu'à 3 ans d'existence légale - mais aucune obligation d’existence légale ;</li>
+							<li>pas de modèle économique définitivement validé.</li>
+						</ul>
+					  </div> 
+
+
+					   <h3> Concrètement, qu’est-ce que le mentorat ?</h3>
+					  <div>
+					    <p> 
+						Le mentorat est une forme de service: deux mentors, désignés soit pour leur connaissance 
+						du secteur du projet, soit pour leur capacité à monter un projet, acceptent de suivre le 
+						porteur de projet de septembre 2015 à mai 2016. Suivre un porteur de projet, c’est accepter de 
+						lui accorder, au moins, une heure d’entretien par mois, dans un cadre de bienveillance, de 
+						confiance et de confidentialité. Il est important aussi de souligner que le mentor ne peut 
+						financer le projet dont il est le mentor du moins, pendant la durée de son mandat de mentor. </br>
+
+						Le mentorat permet de sortir le porteur de projet de la solitude de l’entrepreneur, et lui 
+						permet de bénéficier de l’expérience de personnes plus aguerries que lui. </br>
+
+						En mai 2016, une restitution de  ce mentorat est faite, de manière à évaluer son apport.</br>
+
+						Faire gagner du mentorat et non des ressources financières permet, à la fois, de faire gagner 
+						quelque chose de précieux aux projets, mais aussi d’ouvrir plus largement ce concours aux 
+						projets qui ne sont pas directement inscrits dans la sphère marchande: les projets
+						 intrapreneuriaux (inscrits dans l’activité d’une grande entreprise) voire les secteurs qui ont 
+						 vocation à rester dans le giron du service public.
+					    </p>
+					  </div>
+				 
+
+				  <h3>Qu’est-ce que j’ai à gagner ?</h3>
+					  <div>
+					    <p>
+						Même si vous n’êtes pas sélectionnés pour participer à la soirée Pecha Kucha, ou même si vous êtes sélectionnés, 
+						mais pas lauréats, vous aurez:
+							<ul>
+								<li>reçu un retour bref mais précis sur votre projet ;</li>
+								<li>fait circuler votre projet;</li>
+								<li>appris un nouvel exercice;</li>
+							</ul>
+						Certes, les normaliens ne sont pas les plus mal armés pour affronter le monde de l’innovation mais,
+						en même temps, ils peuvent manquer de confiance en eux. Ils peuvent aussi manquer d’un nécessaire sens de 
+						la communication. Ce concours a pour fin de les aider à essayer, tenter, expérimenter.
+					 	</p>
+					  </div>
+				 
+
+				  <h3>Comment participer ? </h3>
+					  <div>
+					    <p>
+					    Pour soumettre votre projet, il suffit d’envoyer, dans un résumé de votre projet, d’une page, au format 
+					    PDF, expliquant en moins de 1000 signes votre projet. Avec, sur ce document, le nom de votre projet, 
+					    votre logo (s’il existe!), et vos coordonnées. Attention: il faut faire cela avant le 25 août 2015, à 
+					    minuit. N’oubliez pas, dans votre message, d’indiquer qui, dans votre équipe, est membre de la 
+					    “communauté normalienne”. Fin août, vous saurez si vous êtes sélectionné pour participer au premier 
+					    Pecha Kucha du CNE, qui aura lieu le jeudi 8 octobre.
+					    </p>
+					  </div>
+
+
+				  <h3>A quoi ressemblera la soirée Pecha Kucha pour les finalistes ?</h3>
+					  <div>
+					    <p>
+					    Lors d’un dîner en septembre 2015, devant les dizaines de membres du CNE et un jury d’experts, les 
+					    finalistes pourront présenter leur projet sous la forme d’un pecha kucha: 20 images, affichées pendant 
+					    20 secondes au maximum chacune. Soit 6 minutes et 40 secondes, pour convaincre, et montrer que ce projet 
+					    doit être soutenu. Ensuite, l’assemblée vote et détermine son lauréat. De leurs côtés, les experts, qui 
+					    connaissent le résultat du vote de l’assemblée, se retirent pour délibrérer et choisir leur lauréat. Le 
+					    jury peut choisir le même projet que l’assemblée ou un autre projet. Le ou les lauréat(s) se voient 
+					    désigner deux mentors, qui s’engagent à les suivre pendant un an.
+					    </p>
+					  </div>  
+
+
+					  <h3>Mais pourquoi avoir pris une image de Gemini pour illustrer cette page ?</h3>
+					  <div>
+					    <p> 
+						Il y a quelques semaines, le romancier américain de science-fiction Neal Stephenson, a attiré 
+						l’attention de ses lecteurs sur un fait invisible mais inquiétant: selon lui, les jeunes gens les 
+						plus brillants de notre époque, s’ils avaient vécu dans les années 50 ou 60, auraient construit des 
+						vaisseaux spatiaux, ou toutes sortes de choses utiles à l’humanité, mais, de nos jours, ils 
+						s’installent dans la Silicon Valley, avec l’espoir de créer une application qui n’aidera en rien 
+						l’humanité, mais leur permettra de faire fortune rapidement. Pour redonner un peu d’ambition et 
+						de merveilleux à l’innovation, il est bon de se souvenir que nous sommes capables de grandes choses, 
+						comme de nous lancer à la conquête de l’espace. D’où cette petite image des plans de Gemini.
+					    </p>
+					  </div>
+				 
+
+				  <h3>J’ai bien une question, mais elle n’a pas sa réponse dans cette Foire Aux Questions. Que faire ?</h3>
+					  <div>
+					    <p>
+					    Adressez-nous un message grâce au formulaire de bas de page, et nous pourrons améliorer cette 
+					    FAQ grâce à vous !
+					 	</p>
+					  </div>
+				 
+
+				  <h3>J’aimerais être tenu informé des progrès de ce concours. Comment est-ce possible ?</h3>
+					  <div>
+					    <p>
+					    Il suffit de s’abonner à cette lettre d’information.
+					    </p>
+					  </div>
+				  
+				</div>
+ 
+
+
+
+
+
+<!-- $FORMULAIRE -->
+
+			<section id="contact">
+				<h2 class="titre_partie2">Contact</h2>
+					<form>
+				
+				<!-- contenu... -->
+
+
+					</form>
+			</section>
+
+
+
+
+
+
+<!-- $FOOTER -->
+
+
+
+
+
+<!--Fin de la page -->
+	</div> 
+</div>
+
+
+</body>
+</html>
