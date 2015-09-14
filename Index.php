27bd4c35f5600cfd4b1eccbb285e9572f229ec3a
Suffren <regis.cleis@hotmail.fr>
2015-09-14 19:22:36 +0200

    remplacement des liens sociaux par partage

--- Index.php
+++ Index.php
@@ -29,21 +29,26 @@
 
 
 <body>
+<!-- PLUGIN bouton partage FACEBOOK -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.4";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div class="container-fluid">
 
+
 <!-- $HEADER -->
-
-
 <?php
 	include("header.php")
 ?>
 
 
 	<div class="wrap">
-
-
 <!-- $ACTUALITES -->
-
 		<section id="actualites">
 			<div class="double_titre">
 				<h2 class="titre_partie">Les dernières actualités</h2>
