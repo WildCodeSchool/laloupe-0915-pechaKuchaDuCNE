c1e0750bda097f4fc62f154ed8d6a8205dfb9fad
Rabin Julien <julienr114@gmail.com>
2015-09-14 13:26:22 +0200

    creation header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<header class="row">
+  <nav class="col-md-offset-3 col-md-6 text-right">
+    <ul>
+      <a href="#actualites"><li class="menu">Actualités</li></a>
+      <a href="#concours"><li class="menu">Concours</li></a>
+      <a href="#laureats"><li class="menu">Lauréats</li></a>
+      <a href="#partenaires"><li class="menu">Partenaires</li></a>
+      <a href="#FAQ"><li class="menu">FAQ</li class="menu"></a>
+      <a href="#contact"><li class="menu">Contact</li></a>
+    </ul>
+  </nav>
+</header>
