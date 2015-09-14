45a7cd8f0d669bfc428356861965a8a6ede35bf7
Suffren <regis.cleis@hotmail.fr>
2015-09-14 13:41:09 +0200

    ajouts de liens et ajustements

--- Index.php
+++ Index.php
@@ -48,7 +48,7 @@
 
 		<h1>Prix des start-up normaliennes</h1>
 
-		<img src="header.jpg"alt="photo header"/>
+		<img src="ressources/header.jpg"alt="photo header"/>
 
 	<div class="wrap">
 
@@ -435,49 +435,60 @@
 
 
 
+<!-- $pre-FOOTER -->
+<div class="container-fluid">
+  <div class="row pre-footer">
+    <p class="text-center">Prix des start-up Normaliennes - Tout droit réservés<br />
+      Site créé par les élèves de la Wild Code School</p>
+  </div>
+</div>
+        
 
+
+
+
+</div>
 
 <!-- $FOOTER -->
 <div class="container-fluid">
   <div class="row">
     <div class="footer">
 
-        <div class="col-sm-offset-1 col-sm-3 centrage">
+        <div class="col-sm-offset-2 col-sm-2 centrage">
           <h4>Plan du site</h4>
-          <span>
-            <li><a href="#">Actualités</a></li>
-            <li><a href="#">Concours</a></li>
-            <li><a href="#">Les lauréats</a></li>
-            <li><a href="#">Les partenaires</a></li>
-            <li><a href="#">La FAQ</a></li>
-            <li><a href="#">Contact</a></li>
-          </span>
+	          <span>
+	            <li><a href="#">Actualités</a></li>
+	            <li><a href="#">Concours</a></li>
+	            <li><a href="#">Les lauréats</a></li>
+	            <li><a href="#">Les partenaires</a></li>
+	            <li><a href="#">La FAQ</a></li>
+	            <li><a href="#">Contact</a></li>
+	          </span>
         </div>
 
-      <div class="col-sm-offset-1 col-sm-3">
-        <h4>Mentions légales</h4>
-        <span>
-          <li><a href="#">- Club des Normaliens dans l'Entreprise</a></li>
-          <li>- Association loi 1901</li>
-          <li>- 4, rue de Presbourg, 75116 Paris</li>
-          <li>- David Meulemans, Secrétaire général</li>
-          <li>- Responsable de la publication: CNE</li>
-          <li><a href="#">cne.editorial@gmail.com</a></li>
-        </span>
-      </div>
+      	<div class="col-sm-offset-1 col-sm-3">
+          <h4>Mentions légales</h4>
+	        <span>
+	          <li><a href="#">- Club des Normaliens dans l'Entreprise</a></li>
+	          <li>- Association loi 1901</li>
+	          <li>- 4, rue de Presbourg, 75116 Paris</li>
+	          <li>- David Meulemans, Secrétaire général</li>
+	          <li>- Responsable de la publication: CNE</li>
+	          <li><a href="#">cne.editorial@gmail.com</a></li>
+	        </span>
+      	</div>
 
        <div class="col-sm-offset-1 col-sm-3">
-        <h4>Réseaux sociaux:</h4>
-        <span>
-          <li class="align"><img class="logo" src="logo/circle-facebook.png"></a></li>
-          <li class="align"><img class="logo" src="logo/circle-twitter.png"></a></li>
-          <li class="align"><img class="logo" src="logo/circle-tumblr.png"></a></li>
-          <li class="align"><img class="logo" src="logo/circle-linkedin.png"></a></li>
-        </span>
+         <h4>Réseaux sociaux:</h4>
+	        <span>
+	          <li class="align"><a href="#"><img class="social_icons" src="ressources/circle-facebook.png"></a></li>
+	          <li class="align"><a href="#"><img class="social_icons" src="ressources/circle-twitter.png"></a></li>
+	          <li class="align"><a href="#"><img class="social_icons" src="ressources/circle-tumblr.png"></a></li>
+	          <li class="align"><a href="#"><img class="social_icons" src="ressources/circle-linkedin.png"></a></li>
+	        </span>
 
-        <p id="space">Logo réalisé par: Julien Rabin<br />
+        	<p id="space">Logo réalisé par: Etienne Rabin<br />
           Symboles: The Noun-Project</p>
-
       </div>
 
     </div>
@@ -488,7 +499,6 @@
 
 
 <!--Fin de la page -->
-	</div> 
 </div>
 
 
