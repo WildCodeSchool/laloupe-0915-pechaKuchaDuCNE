2c912bb852eb565a126a7fbc2a1a7b5653a3c5fd
Rabin Julien <julienr114@gmail.com>
2015-09-14 17:51:10 +0200

    modification du header

--- header.php
+++ header.php
@@ -1,15 +1,21 @@
 <header class="row">
-  <div class="col-md-offset-2 col-md-1"><img src="ressources/logo4.png" width="90px">
+  <div class="col-md-offset-2 col-md-1"><a href="index.php"><img src="ressources/logo4.png" width="90px"></a>
   </div>
   <nav class="col-md-7 text-right">
-      <a href="#actualites"><li>Actualités</li></a>
-      <a href="#concours"><li>Concours</li></a>
-      <a href="#laureats"><li>Lauréats</li></a>
-      <a href="#partenaires"><li>Partenaires</li></a>
-      <a href="#FAQ"><li>FAQ</li></a>
-      <a href="#contact"><li>Contact</li></a>
+      <a href="#actualites"><li class="menu">Actualités</li></a>
+      <a href="#concours"><li class="menu">Concours</li></a>
+      <a href="#laureats"><li class="menu">Lauréats</li></a>
+      <a href="#partenaires"><li class="menu">Partenaires</li></a>
+      <a href="#FAQ"><li class="menu">FAQ</li></a>
+      <a href="#contact"><li class="menu">Contact</li></a>
   </nav>
 </header>
 <div class="row font1">
-  <h1> LE PRIX DES START-UP</br>NORMALIENNE </h1>
+  <div class="row">
+    <h1 class="logotop text-center">LE PRIX DES STARTUPS</br>
+  </div>
+  <div class="row">
+    <h1 class="logobottom text-center">NORMALIENNES</h1>
+  </div>
+  </div>
 </div>
