f6e3d66faf572e4bb7127df2b8d53411b8bed63a
Rabin Julien <julienr114@gmail.com>
2015-09-15 17:27:37 +0200

    vignette reseau sociaux

--- archives_actualites.php
+++ archives_actualites.php
@@ -6,6 +6,7 @@
 
 	<link href='https://fonts.googleapis.com/css?family=Roboto:500,300,400' rel='stylesheet' type='text/css'>
 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
 	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
--- header.php
+++ header.php
@@ -1,12 +1,16 @@
 <header class="row">
-  <div class="col-md-offset-1 col-md-1"><a href="index.php"><img src="ressources/logo4.png" width="90px"></a>
+  <div class="col-lg-offset-1 col-lg-1"><a href="index.php"><img src="ressources/logo4.png" width="90px"></a>
   </div>
-  <nav class="col-md-7 text-right">
+  <nav class="col-lg-6 text-right">
       <a href="index.php#actualites"><li class="menu">Actualités</li></a>
       <a href="index.php#concours"><li class="menu">Concours</li></a>
       <a href="index.php#laureats"><li class="menu">Lauréats</li></a>
       <a href="index.php#partenaires"><li class="menu">Partenaires</li></a>
-      <a href="index.php#FAQ"><li class="menu">FAQ</li></a>
+      <a href="faq.php"><li class="menu">FAQ</li></a>
       <a href="index.php#contact"><li class="menu">Contact</li></a>
   </nav>
+  <div class="col-lg-3 text-right">
+    <a href="https://fr.linkedin.com/groups/Club-Normaliens-lEntreprise-25050/about"><i class="fa fa-linkedin-square fa-2x"></i></a>
+    <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+  </div>
 </header>
